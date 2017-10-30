4e5a9040140cd0dadd1629e4f098070d23326301
Jip Sterk <jipsterk1998@gmail.com>
2017-10-30 19:31:07 +0100

    Initial commit

---
+++ src/components/main.tsx
@@ -0,0 +1,10 @@
+import './main.scss';
+
+import * as React from 'react';
+
+export const Main = ()=> (
+    <div>
+        <h1>Welcome to the React and Webpack template!</h1>
+        <img className="react-image" />
+    </div>
+);
